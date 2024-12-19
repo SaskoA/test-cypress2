b66b14c7722e4de4a21edfb718be2b5277149ade
SaskoA <arsovski_sasko@hotmail.com>
2024-12-19 11:56:21 +0100

    Text Box

---
+++ cypress/e2e/homework3/textClass.js
@@ -0,0 +1,12 @@
+class textClass {
+    clickButtonByLocator(buttonElement) {
+        cy.get(buttonElement).click()
+    }
+    clickButtonByText(buttonElement, buttonText) {
+        cy.get(buttonElement).contains(buttonText).click()
+    }
+    sendText(locator, text) {
+        cy.get(locator).type(text)
+    }
+}
+module.exports = new textClass()
